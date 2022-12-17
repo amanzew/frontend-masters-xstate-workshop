ef2172aa921f633fb923831e95306d870d7a0ba0
amanzew <amanzew@gmail.com>
2022-12-17 17:38:04 +0300

    eventobject

--- src/00/index.js
+++ src/00/index.js
@@ -14,4 +14,87 @@ const user = {
   interests: ['piano', 'state machines'],
 };
 
+function transition(state,event){
+  switch (state) {
+    case 'idle':
+      switch(event){
+        case 'fetch': return 'pending'
+        default: return state
+      }
+    case 'pending':
+      switch(event){
+        case 'resolve': return 'resolved'
+        case 'reject': return 'rejected'
+        default: return state
+      }
+    case 'resolved':
+    case 'rejected':  
+    default:return state;
+  }
+}
+
+const machine={
+  initial:'idle',
+  states:{
+    idle:{
+      on:{
+        fetch:'pending'
+      }
+    },
+    pending:{
+      on:{
+        resolve:'resolved',
+        reject:'rejected'
+      }
+    },
+    resolved:{},
+    rejected:{},
+  }
+}
+
+const transitiona=(state,event)=>{
+  return machine.states[state]?.on?.[event] || state
+}
+
+let currentState=machine.initial;
+const send=(event)=>{
+  const nextstate=transitiona(currentState,event)
+  console.log(nextstate);
+  currentState=nextstate;
+}
 output(user);
+window.send=send;
+
+const feedbackMachine=createMachine({
+  initial:'question',
+  states:{
+    question:{
+      on:{
+        CLICK_GOOD:'thanks',
+        CLICK_BAD:'form'
+      }
+    },
+    form:{
+      on:{
+        SUBMIT:'thanks',
+      }
+    },
+    thanks:{
+      on:{
+        CLOSE:'closed',
+      }
+    },
+    closed:{
+
+    }
+  }
+})
+const clickGoodEvent={
+  type:'CLICK_GOOD',
+  time:Date.now()
+}
+const nextstate=feedbackMachine.transition(
+  feedbackMachine.initial,clickGoodEvent
+)
+console.log(nextstate);
+//console.log(transitiona('pending','a'));
--- src/01/index.js
+++ src/01/index.js
@@ -1,25 +1,55 @@
 const elBox = document.querySelector('#box');
+const elDiv = document.querySelector('#div');
+
 
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
+  switch (state) {
+    case 'inactive':
+      switch(event){
+        case 'click': return 'active'
+        default:return state;
+      }
+    case 'active':
+      switch(event){
+        case 'click': return 'inactive'
+        default:return state;
+      }
+    default:return state;
   }
+}
+const machine={
+  initital:'inactive',
+  states:{
+    inactive:{
+      on:{
+        click: 'active'
+      }
+    },
+    active:{
+      on:{
+      click: 'inactive'
+      }
+    }
+  }
+}
+const transitiona=(state,event)=>{
+ return machine.states[state]?.on?.[event] || event
 }
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = 'inactive';
 
 function send(event) {
   // Determine the next value of `currentState`
+  const nextState=transition(currentState,event)
+  currentState=nextState
 
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
   // send a click event
+  send('click')
 });
